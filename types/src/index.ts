cdbb879036f4584fa50de5afb1f2587ba3a7eb76
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-25 21:37:59 -0300

    keyof example

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,30 +1,13 @@
-// Classes that define a Symbol.iterator property can implement the Iterable<T> interface, which allows
-// iteration without needing to call a method or read a property
-
 import { City, Person, Product, Employee } from "./dataTypes";
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-type shapeType = { name: string };
-class Collection<T extends shapeType> implements Iterable<T> {
-  private items: Map<string, T>;
-  constructor(initialItems: T[] = []) {
-    this.items = new Map<string, T>();
-    this.add(...initialItems);
-  }
-  add(...newItems: T[]): void {
-    newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
-  }
-  get(name: string): T {
-    return this.items.get(name);
-  }
-  get count(): number {
-    return this.items.size;
-  }
-  [Symbol.iterator](): Iterator<T> {
-    return this.items.values();
-  }
+
+function getValue<T, K extends keyof T>(item: T, keyname: K) {
+  console.log(`Value: ${item[keyname]}`);
 }
-let productCollection: Collection<Product> = new Collection(products);
-console.log(`There are ${productCollection.count} products`);
-[...productCollection].forEach((p) =>
-  console.log(`Product: ${p.name}, ${p.price}`)
-);
+
+let p = new Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+
+let e = new Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
