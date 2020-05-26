15b6d522d43d0f7b691881c738daca695040ede1
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-25 21:46:04 -0300

    Implemented in Collection class

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,13 +1,31 @@
 import { City, Person, Product, Employee } from "./dataTypes";
-
-function getValue<T, K extends keyof T>(item: T, keyname: K) {
-  console.log(`Value: ${item[keyname]}`);
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+//type shapeType = { name: string };
+class Collection<T, K extends keyof T> implements Iterable<T> {
+  private items: Map<T[K], T>;
+  constructor(initialItems: T[] = [], private propertyName: K) {
+    this.items = new Map<T[K], T>();
+    this.add(...initialItems);
+  }
+  add(...newItems: T[]): void {
+    newItems.forEach((newItem) =>
+      this.items.set(newItem[this.propertyName], newItem)
+    );
+  }
+  get(key: T[K]): T {
+    return this.items.get(key);
+  }
+  get count(): number {
+    return this.items.size;
+  }
+  [Symbol.iterator](): Iterator<T> {
+    return this.items.values();
+  }
 }
-
-let p = new Product("Running Shoes", 100);
-getValue(p, "name");
-getValue(p, "price");
-
-let e = new Employee("Bob Smith", "Sales");
-getValue(e, "name");
-getValue(e, "role");
+let productCollection: Collection<Product, "name"> = new Collection(
+  products,
+  "name"
+);
+console.log(`There are ${productCollection.count} products`);
+let itemByKey = productCollection.get("Hat");
+console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
