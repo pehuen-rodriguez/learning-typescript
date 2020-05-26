9ada54e8db16668d0d25495be485e4df858ac381
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-25 21:34:40 -0300

    Iterable example

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,50 +1,30 @@
+// Classes that define a Symbol.iterator property can implement the Iterable<T> interface, which allows
+// iteration without needing to call a method or read a property
+
 import { City, Person, Product, Employee } from "./dataTypes";
-
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 type shapeType = { name: string };
-
-interface Collection<T extends shapeType> {
-  add(...newItems: T[]): void;
-  get(name: string): T;
-  count: number;
-}
-
-abstract class ArrayCollection<T extends shapeType> implements Collection<T> {
-  protected items: T[] = [];
-
+class Collection<T extends shapeType> implements Iterable<T> {
+  private items: Map<string, T>;
+  constructor(initialItems: T[] = []) {
+    this.items = new Map<string, T>();
+    this.add(...initialItems);
+  }
   add(...newItems: T[]): void {
-    this.items.push(...newItems);
+    newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
   }
-
-  abstract get(searchTerm: string): T;
-
+  get(name: string): T {
+    return this.items.get(name);
+  }
   get count(): number {
-    return this.items.length;
+    return this.items.size;
+  }
+  [Symbol.iterator](): Iterator<T> {
+    return this.items.values();
   }
 }
-
-class ProductCollection extends ArrayCollection<Product> {
-  get(searchTerm: string): Product {
-    return this.items.find((item) => item.name === name);
-  }
-}
-class PersonCollection extends ArrayCollection<Person> {
-  get(searchTerm: string): Person {
-    return this.items.find((item) => item.name === name || item.city === name);
-  }
-}
-
-let peopleCollection: Collection<Person> = new PersonCollection();
-peopleCollection.add(
-  new Person("Bob Smith", "London"),
-  new Person("Dora Peters", "New York")
-);
-
-let productCollection: Collection<Product> = new ProductCollection();
-productCollection.add(
-  new Product("Running Shoes", 100),
-  new Product("Hat", 25)
-);
-
-[peopleCollection, productCollection].forEach((c) =>
-  console.log(`Size: ${c.count}`)
+let productCollection: Collection<Product> = new Collection(products);
+console.log(`There are ${productCollection.count} products`);
+[...productCollection].forEach((p) =>
+  console.log(`Product: ${p.name}, ${p.price}`)
 );
